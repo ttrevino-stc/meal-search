572bd882d2a9a9c26e4b8079c813fbb2132f9516
Thomas Trevino <ttrevino@stu.southtexascollege.edu>
2025-05-05 21:45:00 -0500

    Meals By Ingredients Done

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -24,7 +24,7 @@ const searchIngredients = computed(() => {
 </script>
 <template>
     <div class="p-8 pb-0 text-orange-500">
-        <h1 class="text-4xl font-bold mb-4"> Search Meals By ingredients</h1>
+        <h1 class="text-4xl font-bold mb-4"> Search Meals By Ingredients</h1>
     </div>
 
     <!-- {{ ingredients }} -->
